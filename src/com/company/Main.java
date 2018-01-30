257bc305ee47b5c59201d8ffd64cde116499fbf0
Vaidotas <vaidotas.eigelis@gmail.com>
2018-01-30 15:53:31 +0200

    Code 08

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,17 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ivesk kilometrus: ");
+        float atstumas = sc.nextFloat();
+        System.out.println("Ivesk litrus: ");
+        float litrai = sc.nextFloat();
+
+        float vidurkis = (litrai*100)/atstumas;
+        System.out.println("Vidurkis: " + vidurkis);
     }
 }
